d8d7126b2dac534be170df5ac637df69b7f39ea7
Paulo Ricardo Busch <paulo202015@outlook.com.br>
2021-02-11 20:22:02 -0300

    Projeto inicial

---
+++ src/app/App.jsx
@@ -0,0 +1,10 @@
+import './App.css';
+
+import React from 'react';
+
+export default props => (
+  <div>
+    App
+    <i className="fa fa-calendar-check-o"></i>
+  </div>
+);
---
+++ src/index.jsx
@@ -0,0 +1,12 @@
+import 'modules/font-awesome/css/font-awesome.min.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './app/App';
+
+ReactDOM.render(
+  <div>
+    <App />
+  </div>,
+  document.getElementById('app')
+);
