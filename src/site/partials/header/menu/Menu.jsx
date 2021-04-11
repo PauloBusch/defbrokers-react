39ca73372fd338142bc9348f4bfcc1e59f8bf138
Paulo Ricardo Busch <paulo202015@outlook.com.br>
2021-04-11 14:38:45 -0300

    Social icons

--- src/site/partials/header/menu/Menu.jsx
+++ src/site/partials/header/menu/Menu.jsx
@@ -8,16 +8,21 @@ import { Link } from 'react-router';
 
 class Menu extends Component {
   render() {
-    const { name, whatsapp, open, onClick } = this.props;
+    const { name, whatsapp, open, onClick, facebookLabel, facebookLink, instagramLabel, instagramLink } = this.props;
 
     return (
       <ul className={ `menu ${ open ? 'show' : '' }` } onClick={ onClick }>
         <li><Link href="/">Imóveis</Link></li>
         <li><Link to="/#about">{ name }</Link></li>
-        <li className="phone">
-          <a href={ generateAddContactLink(whatsapp) } target="_blank">
-            <i className="fab fa-whatsapp"></i>&nbsp;
-            { whatsapp }
+        <li className="icons">
+          <a href={ generateAddContactLink(whatsapp) } title={ whatsapp } target="_blank">
+            <i className="fab fa-whatsapp"></i>
+          </a>
+          <a  href={ instagramLink } title={ instagramLabel } target="_blank">
+            <i className="fab fa-instagram"></i>
+          </a>
+          <a  href={ facebookLink } title={ facebookLabel } target="_blank">
+            <i className="fab fa-facebook-f"></i>
           </a>
         </li>
       </ul>
